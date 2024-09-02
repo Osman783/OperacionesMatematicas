1b8c3c67b4c30de63eba1ef422515da5a533a2e3
Osman783 <rodasmartinez404@gmail.com>
2024-09-01 22:48:10 -0600

    Agregar archivos de proyecto.

---
+++ OperacionesMatematicas/Program.cs
@@ -0,0 +1,24 @@
+﻿{
+    
+    { 
+    int num1;
+    int num2;
+    int resSuma;
+    int resResta;
+    int resMulti;
+    int resDiv;
+    Console.WriteLine("Ingrese el primer numero");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    resSuma = num1 + num2;
+    resResta = num1 - num2;
+    resMulti = num1 * num2;
+    resDiv = num1 / num2;
+    Console.WriteLine("Suma: " + resSuma);
+    Console.WriteLine("Resta: " + resResta);
+    Console.WriteLine("Multiplicacion: " + resMulti);
+    Console.WriteLine("Division: " + resDiv);
+    Console.WriteLine();
+}
+}
